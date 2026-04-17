bff5e35c5a043622347f918e072921321dcc6f43
JuanLif3 <juanitololilifeloli@gmail.com>
2026-04-17 18:03:02 -0400

    fix(Auth): Implementation of password to authHash

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,16 +1,26 @@
 import api from './api';
 
 export const authService = {
+    // LOGIN
     login: async (email, password) => {
-        const response = await api.post('/auth/login', { email, password });
+        const response = await api.post('/auth/login', { 
+            email: email, 
+            authHash: password // <-- Traducimos 'password' a 'authHash' para el backend
+        });
         return response.data;
     },
-    register: async (userData) => {
-        const response = await api.post('/auth/register', userData);
+
+    // REGISTRO
+    register: async (email, password) => {
+        const response = await api.post('/auth/register', { 
+            email: email, 
+            authHash: password // <-- Traducimos 'password' a 'authHash' para el backend
+        });
         return response.data;
     },
+
+    // LOGOUT (El que hicimos en la Fase 1)
     logout: async () => {
-        // Le avisamos al backend que destruya la cookie
         await api.post('/auth/logout'); 
     }
 };
